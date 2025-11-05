57aad345b399b5c79661acd799dd0c2a470cb1c1
AndreeaMagda <andreea2002@gmail.com>
2025-11-05 10:34:21 +0200

    Alignment

--- app/src/main/java/com/example/hamster_dispenser/MainActivity.kt
+++ app/src/main/java/com/example/hamster_dispenser/MainActivity.kt
@@ -5,13 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,16 +42,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Food dispenser for $name!",
-        fontSize = 30.sp,
-        color = Color.Yellow,
+    Box(
         modifier = Modifier
-            .background(Color.Cyan)
-            .padding(16.dp)
-            .background(Color.Magenta   )
-
+            .size(400.dp)
+            ){
+    Text(
+        text = "Hello $name!",
+        fontSize = 30.sp,
+        color = Color.Blue,
+        modifier= Modifier.align(Alignment.BottomEnd)
     )
+    Text(
+        text = "Welcome to your food dispenser",
+        fontSize = 25.sp,
+        color = Color.Red
+    )}
 }
 
 @Preview(showBackground = true)
