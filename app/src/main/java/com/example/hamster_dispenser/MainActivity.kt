09b3dbe73810f14d61f361e3dce65c31cf62cf22
AndreeaMagda <andreea2002@gmail.com>
2025-11-06 09:49:38 +0200

    Add name to a list

--- app/src/main/java/com/example/hamster_dispenser/MainActivity.kt
+++ app/src/main/java/com/example/hamster_dispenser/MainActivity.kt
@@ -5,8 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,6 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.hamster_dispenser.ui.theme.HamsterDispenserTheme
 
@@ -23,26 +30,42 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HamsterDispenserTheme {
-                var count by remember{
-                    mutableStateOf(0)
+                var name by remember {
+                    mutableStateOf("")
+                }
+                var names by remember {
+                    mutableStateOf(listOf<String>())
                 }
                 Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Text(
-                        text=count.toString(),
-                        fontSize=30.sp
-                    )
-                    Button(onClick = {
-                        count++
-                    }
-                    ) {
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                )
+                {
+                    Row (
+                        modifier = Modifier.fillMaxWidth()
+                    ){
+                        OutlinedTextField(
+                            value=name,
+                            onValueChange = {text ->
+                                name=text
+                            }
+                        )
 
-                        Text(text="Add Food for your hammy")
+                        Button(onClick = {
+                            if(name.isNotBlank()){
+                            names=names +name}
+                        }) {
+                            Text(text="Add")
+                        }
+                    }
+                    LazyColumn {
+                        items(names){ currentName ->
+                            Text(text = currentName)
+                        }
                     }
                 }
+
             }
         }
     }
