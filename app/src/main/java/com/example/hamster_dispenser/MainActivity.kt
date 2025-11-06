b35aef84322380c048a8d73ebe13e027d5d2347c
AndreeaMagda <andreea2002@gmail.com>
2025-11-06 09:38:10 +0200

    Clickable button

--- app/src/main/java/com/example/hamster_dispenser/MainActivity.kt
+++ app/src/main/java/com/example/hamster_dispenser/MainActivity.kt
@@ -1,79 +1,49 @@
 package com.example.hamster_dispenser
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SegmentedButtonDefaults.Icon
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.hamster_dispenser.ui.theme.HamsterDispenserTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             HamsterDispenserTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Hammy",
-                        modifier = Modifier.padding(innerPadding)
+                var count by remember{
+                    mutableStateOf(0)
+                }
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Text(
+                        text=count.toString(),
+                        fontSize=30.sp
                     )
+                    Button(onClick = {
+                        count++
+                    }
+                    ) {
+
+                        Text(text="Add Food for your hammy")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    LazyColumn(modifier= Modifier.fillMaxSize()) {
-        items(10) { i ->
-            Icon(
-                imageVector = Icons.Default.Add,
-                contentDescription = null,
-                modifier= Modifier.size(100.dp)
-            )
-        }
-    }
-
-    LazyRow(modifier= Modifier.fillMaxSize()) {
-        items(10) { i ->
-            Icon(
-                imageVector = Icons.Default.Add,
-                contentDescription = null,
-                modifier= Modifier.size(100.dp)
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HamsterDispenserTheme {
-        Greeting("Hammy")
     }
 }
